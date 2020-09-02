b8eec22df648a1e8d037db7c5fb0c4b6305fb17c
蔡俊辉 <caqizhe@gmail.com>
2020-09-03 07:42:06 +0800

    build: add Dockerfile

--- src/main/java/com/github/caijh/AuthServerApplication.java
+++ src/main/java/com/github/caijh/AuthServerApplication.java
@@ -2,14 +2,10 @@ package com.github.caijh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableAspectJAutoProxy
+@EnableJpaRepositories
 public class AuthServerApplication {
 
     public static void main(String[] args) {
