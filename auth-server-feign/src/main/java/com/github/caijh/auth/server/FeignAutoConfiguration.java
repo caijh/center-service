10d867ee4814b9f5b5882f552bcaa75bc3156d0c
caijunhui <caiqizhe@gmail.com>
2020-10-23 18:31:09 +0800

    fix: modify EnableDiscoveryClient to EnableFeignClients

--- auth-server-feign/src/main/java/com/github/caijh/auth/server/FeignAutoConfiguration.java
+++ auth-server-feign/src/main/java/com/github/caijh/auth/server/FeignAutoConfiguration.java
@@ -1,10 +1,10 @@
 package com.github.caijh.auth.server;
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableDiscoveryClient
+@EnableFeignClients
 public class FeignAutoConfiguration {
 
 }
