2ff3ed512771819105e7a59837dead3f1a090324
caijunhui <caiqizhe@gmail.com>
2020-11-04 19:01:06 +0800

    refactor

--- auth-server-feign/src/main/java/com/github/caijh/auth/server/feign/AuthInfo.java
+++ auth-server-feign/src/main/java/com/github/caijh/auth/server/feign/AuthInfo.java
@@ -2,14 +2,21 @@ package com.github.caijh.auth.server.feign;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class AuthInfo {
 
+    @JsonProperty("user_id")
     private String userId;
+
+    @JsonProperty("user_name")
     private String userName;
+
+    @JsonProperty("client_id")
     private String clientId;
+
     private List<String> scope;
 
 }
