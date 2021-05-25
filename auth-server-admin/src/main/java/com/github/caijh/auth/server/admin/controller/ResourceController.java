598b9f9153c84798c1a67ca3673a25256a0a52de
蔡俊辉 <caqizhe@gmail.com>
2021-05-25 23:48:47 +0800

    build: 2.3.2-1.0.1-SNAPSHOT

---
+++ auth-server-admin/src/main/java/com/github/caijh/auth/server/admin/controller/ResourceController.java
@@ -0,0 +1,8 @@
+package com.github.caijh.auth.server.admin.controller;
+
+import com.github.caijh.framework.web.controller.BaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController extends BaseController {
+}
--- auth-server-admin/src/main/java/com/github/caijh/auth/server/admin/service/impl/UserServiceImpl.java
+++ auth-server-admin/src/main/java/com/github/caijh/auth/server/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
-package com.github.caijh.auth.server.admin.service;
+package com.github.caijh.auth.server.admin.service.impl;
 
 import com.github.caijh.auth.server.admin.repository.UserRepository;
+import com.github.caijh.auth.server.admin.service.UserService;
 import com.github.caijh.auth.server.entity.User;
 import com.github.caijh.framework.data.BaseServiceImpl;
 import org.springframework.stereotype.Service;
