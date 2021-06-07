1cdf65a12bf6bd564fc09be09cd557ff9b786420
蔡俊辉 <caijunhui@pcitech.com>
2021-06-07 15:56:26 +0800

    auth-server-admin-ui独立成一个git

--- auth-server-admin-ui/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-    isMobile: state => state.app.isMobile,
-    lang: state => state.app.lang,
-    theme: state => state.app.theme,
-    color: state => state.app.color,
-    welcome: state => state.user.welcome,
-}
-
-export default getters
